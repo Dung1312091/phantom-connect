70b1e789849fc193d3fc7aa633fde7733f902340
dungle <quangdung100194@gmail.com>
2024-07-18 13:12:18 +0700

    fix:ton connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const App = () => {
-  const [values, setValues] = useState<any>({
-    value: 1,
-  });
+const Test = () => {
+  useEffect(() => {
+    localStorage.setItem("test", "1");
+    return () => localStorage.removeItem("test");
+  }, []);
   return (
     <div>
-      <div>value: {values.value}</div>
       <button
-        onClick={() => {
-          setValues(null);
-        }}
+        onClick={() =>
+          (window as any).Telegram.WebApp.openTelegramLink(
+            "https://t.me/mpc_wallet_connect_bot/tobi_wallet?start_app=1"
+          )
+        }
       >
-        Reset value
+        Click
       </button>
+    </div>
+  );
+};
+const App = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <h3>App</h3>
+      <div>{localStorage.getItem("test")}</div>
+      <button onClick={() => setShow(!show)}>Click show</button>
+      {show && <Test />}
     </div>
   );
 };
